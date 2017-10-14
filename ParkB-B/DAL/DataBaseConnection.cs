665d7f28d05632d078aa9a001249572c18898153
Philippe Jacob <phildesalondepce@hotmail.fr>
2017-10-14 17:49:24 +0200

    Connexion à la base de donnée mais branche de TEST, ATTNETION

--- ParkB-B/DAL/DataBaseConnection.cs
+++ ParkB-B/DAL/DataBaseConnection.cs
@@ -9,14 +9,54 @@ namespace ParkB_B.DAL
 {
     class DataBaseConnection
     {
-        public void Connect()
+        MySqlConnection connection = null;
+        public MySqlConnection Connection
         {
-            string add = "datasource=243b8f54-a2a4-4bc5-bbc4-0368ed6aacf2.parkpnp-4699.mysql.dbs.scalingo.com;";
-            string port = "port=31244;";
-            string user_name = "username=parkpnp_4699";
-            string pw = "password=YT1QP7Ult2t2tCOeTIol";
-            MySqlConnection connection = new MySqlConnection(add+port+"Initial Catalog='test';"+user_name+pw);
+            get { return connection; }
+        }
 
+        private string databaseName = string.Empty;
+        public string DatabaseName
+        {
+            get { return databaseName; }
+            set { databaseName = value; }
+        }
+
+        private DataBaseConnection() { }
+
+        private static DataBaseConnection _instance = null;
+        public static DataBaseConnection Instance()
+        {
+            if (_instance == null)
+                _instance = new DataBaseConnection();
+            return _instance;
+        }
+
+        public bool IsConnect()
+        {
+            bool result = true;
+            if (Connection == null)
+            {
+                //if (String.IsNullOrEmpty(databaseName))
+                //    result = false;
+                //string connstring = string.Format("Server=localhost; database={0}; UID=UserName; password=your password", databaseName);
+
+                string add = "Server=243b8f54-a2a4-4bc5-bbc4-0368ed6aacf2.parkpnp-4699.mysql.dbs.scalingo.com;";
+                string port = "Port=31244;";
+                string user_name = "Uid=parkpnp_4699;";
+                string pw = "Pwd=YT1QP7Ult2t2tCOeTIol;";
+                connection = new MySqlConnection(add + port + "Database = parkpnp_4699;" + user_name + pw);
+                connection.Open();
+                result = true;
+            }
+
+            return result;
+        }
+
+
+        public void Close()
+        {
+            this.connection.Close();
         }
     }
 }
--- ParkB-B/DAL/RequestData.cs
+++ ParkB-B/DAL/RequestData.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,24 @@ namespace ParkB_B.DAL
 {
     class RequestData
     {
+        DataBaseConnection db = DataBaseConnection.Instance();
+
+        public void test()
+        {
+            if (db.IsConnect())
+            {
+                //suppose col0 and col1 are defined as VARCHAR in the DB
+                string query = "SELECT * FROM Company";
+                var cmd = new MySqlCommand(query, db.Connection);
+                var reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    string someStringFromColumnZero = reader.GetString(0);
+                    string someStringFromColumnUn = reader.GetString(1);
+                    Console.WriteLine(someStringFromColumnZero + " : " + someStringFromColumnUn);
+                }
+            }
+            db.Close();
+        }
     }
 }
