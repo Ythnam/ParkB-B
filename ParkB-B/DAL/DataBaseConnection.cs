1607d54e4c9fe317ba1b3d26d0625e83dd896d19
Philippe Jacob <phildesalondepce@hotmail.fr>
2017-10-14 17:01:54 +0200

    commit

---
+++ ParkB-B/DAL/DataBaseConnection.cs
@@ -0,0 +1,22 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkB_B.DAL
+{
+    class DataBaseConnection
+    {
+        public void Connect()
+        {
+            string add = "datasource=243b8f54-a2a4-4bc5-bbc4-0368ed6aacf2.parkpnp-4699.mysql.dbs.scalingo.com;";
+            string port = "port=31244;";
+            string user_name = "username=parkpnp_4699";
+            string pw = "password=YT1QP7Ult2t2tCOeTIol";
+            MySqlConnection connection = new MySqlConnection(add+port+"Initial Catalog='test';"+user_name+pw);
+
+        }
+    }
+}
